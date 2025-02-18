47b99dfc7d2b10f6c029119ff57a644063eac098
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-02-18 14:54:43 +0500

    Change theme colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -7,10 +7,10 @@ final ThemeData themeData = ThemeData(
 
 final ColorScheme colorScheme = ColorScheme.light().copyWith(
   surface: Color.fromRGBO(5, 30, 37, 1),
-  primary: Colors.white,
-  secondary: Color.fromRGBO(28, 50, 57, 1)
+  primary: Color.fromRGBO(28, 50, 57, 1),
 );
 
 final AppBarTheme appBarTheme = AppBarTheme(
   color: Color.fromRGBO(0, 92, 117, 1),
+  foregroundColor: Colors.white
 );
